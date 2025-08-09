6bfac16b601023a9f58ba7e3089cdfa37c9c3ccc
vybraan <ailtonbauque@outlook.com>
2025-08-09 11:30:46 +0200

    feat: add certs generation utility

---
+++ internal/certgen/certgen.go
@@ -0,0 +1,69 @@
+package certgen
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/hex"
+	"encoding/pem"
+	"math/big"
+	"strings"
+	"time"
+)
+
+func GenerateSelfSignedCert() (tls.Certificate, string, error) {
+	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return tls.Certificate{}, "", err
+	}
+
+	// Generate public key bytes in uncompressed format (0x04 | X | Y)
+	pubkey := append([]byte{0x04}, priv.PublicKey.X.Bytes()...)
+	pubkey = append(pubkey, priv.PublicKey.Y.Bytes()...)
+	pubKeyHex := FormatPublicKey(pubkey)
+
+	// Create a template for a self-signed cert
+	serialNumber, _ := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			CommonName: "localhost",
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(365 * 24 * time.Hour), // valid for 1 year
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		return tls.Certificate{}, "", err
+	}
+
+	// PEM encode the certificate and private key
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	privBytes, _ := x509.MarshalECPrivateKey(priv)
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: privBytes})
+
+	// Load into tls.Certificate
+	cert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		return tls.Certificate{}, "", err
+	}
+
+	return cert, pubKeyHex, nil
+}
+
+// formatPublicKey formats the public key into colon-separated uppercase hex
+func FormatPublicKey(b []byte) string {
+	hexStr := hex.EncodeToString(b)
+	var parts []string
+	for i := 0; i < len(hexStr); i += 2 {
+		parts = append(parts, strings.ToUpper(hexStr[i:i+2]))
+	}
+	return strings.Join(parts, ":")
+}
