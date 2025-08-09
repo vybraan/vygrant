457021a1ef6c2fbfdf041fab326db781f3100689
vybraan <ailtonbauque@outlook.com>
2025-08-09 12:25:42 +0200

    refactor: use fmt.appendf for efficient response and error writing

--- internal/daemon/response.go
+++ internal/daemon/response.go
@@ -5,10 +5,10 @@ import (
 	"net"
 )
 
-func writeError(conn net.Conn, format string, args ...interface{}) {
-	conn.Write([]byte(fmt.Sprintf("ERROR: "+format+"\n", args...)))
+func writeError(conn net.Conn, format string, args ...any) {
+	conn.Write(fmt.Appendf(nil, "ERROR: "+format+"\n", args...))
 }
 
-func writeResponse(conn net.Conn, format string, args ...interface{}) {
-	conn.Write([]byte(fmt.Sprintf(format+"\n", args...)))
+func writeResponse(conn net.Conn, format string, args ...any) {
+	conn.Write(fmt.Appendf(nil, format+"\n", args...))
 }
