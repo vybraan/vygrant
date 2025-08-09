3d590c860cc00239c93ec1711853ebeca0d2edd0
vybraan <ailtonbauque@outlook.com>
2025-08-09 11:31:44 +0200

    feat: implement configuration management

---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Account struct {
+	AuthURI       string            `toml:"auth_uri"`
+	TokenURI      string            `toml:"token_uri"`
+	ClientID      string            `toml:"client_id"`
+	ClientSecret  string            `toml:"client_secret"`
+	RedirectURI   string            `toml:"redirect_uri"`
+	Scopes        []string          `toml:"scopes"`
+	AuthURIFields map[string]string `toml:"auth_uri_fields"`
+}
+
+type Config struct {
+	HTTPSListen string              `toml:"https_listen"`
+	HTTPListen  string              `toml:"http_listen"`
+	Accounts    map[string]*Account `toml:"account"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	var cfg Config
+	if _, err := toml.DecodeFile(path, &cfg); err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("config file not found at %s — please run `vygrant init` or create one manually", path)
+		}
+	}
+	return &cfg, nil
+}
+
+func LoadConfigFromEnv() (*Config, error) {
+	confPath := os.Getenv("VYGRANT_CONFIG")
+	if confPath == "" {
+		return nil, fmt.Errorf("could not load env config")
+	}
+	return LoadConfig(confPath)
+}
