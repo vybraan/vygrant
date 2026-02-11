b8e561a9283e2a036f0fdccc7906d4844b007d4a
Ailton Baúque <ailtonbauque@outlook.com>
2026-02-11 19:08:53 +0200

    chore(version): bump version to 0.6.1

--- cmd/version.go
+++ cmd/version.go
@@ -11,7 +11,7 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of Vygrant",
 	Long:  `All software has versions. This is Vygrant's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Vygrant - OAuth2 authentication daemon v0.6.0")
+		fmt.Println("Vygrant - OAuth2 authentication daemon v0.6.1")
 	},
 }
 
